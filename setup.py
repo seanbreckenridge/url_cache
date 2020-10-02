64d4459830acca4a322960f4c9d59191fcd35e32
Sean Breckenridge <seanbrecke@gmail.com>
2020-10-02 02:42:59 -0700

    autoformat setup

--- setup.py
+++ setup.py
@@ -9,30 +9,30 @@ requirements = [
 ]
 
 # Use the README.md content for the long description:
-with io.open('README.md', encoding='utf-8') as fo:
+with io.open("README.md", encoding="utf-8") as fo:
     long_description = fo.read()
 
 setup(
-    name='url_metadata',
+    name="url_metadata",
     version="0.1.0",
-    url='https://github.com/seanbreckenridge/url_metadata',
-    author='Sean Breckenridge',
-    author_email='seanbrecke@gmail.com',
-    description=('''cache metadata from URLs'''),
+    url="https://github.com/seanbreckenridge/url_metadata",
+    author="Sean Breckenridge",
+    author_email="seanbrecke@gmail.com",
+    description=("""cache metadata from URLs"""),
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    license='MIT',
-    packages=find_packages('src'),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=find_packages("src"),
     zip_safe=False,
-    package_dir={'': 'src'},
+    package_dir={"": "src"},
     install_requires=requirements,
-    keywords='url data youtube subtitles',
+    keywords="url data youtube subtitles",
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )
