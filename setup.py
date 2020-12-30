ed4300c6e8613d683a9d9f60a539b9ae9d70e3c8
Sean Breckenridge <seanbrecke@gmail.com>
2020-12-29 16:17:48 -0800

    bump version
    lots of structural changes in this version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with io.open("README.md", encoding="utf-8") as fo:
 
 setup(
     name="url_metadata",
-    version="0.1.5",
+    version="0.1.6",
     url="https://github.com/seanbreckenridge/url_metadata",
     author="Sean Breckenridge",
     author_email="seanbrecke@gmail.com",
